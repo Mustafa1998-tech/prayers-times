85989ce0bd034885c8f9c75c7e0b52dcfdf82b11
Mustafa Ahmed <mustafaanwar1998@gmail.com>
2025-10-07 08:13:00 +0300

    Fix GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/prayers-times/',
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? '/prayers-times/' : '/';
+  return {
+    base,
+    plugins: [react()],
+  };
+});
